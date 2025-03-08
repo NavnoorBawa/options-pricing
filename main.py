49fef4fc21c27b7123fa1434f906f32c62b8db68
NavnoorBawa <128870087+NavnoorBawa@users.noreply.github.com>
2025-03-08 11:41:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1297,7 +1297,16 @@ def main():
     """, unsafe_allow_html=True)
 
     # Author Section
-    st.markdown("<a href='https://www.linkedin.com/in/navnoorbawa/' target='_blank'>Created by Navnoor Bawa</a>", unsafe_allow_html=True)
+    st.markdown("""
+        <div style="background-color: #1E2124; padding: 15px; border-radius: 10px; display: flex; align-items: center; width: fit-content; margin-bottom: 20px;">
+            <div style="margin-right: 15px;">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40">
+                    <path fill="#0A66C2" d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 00.1.4V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                </svg>
+            </div>
+            <a href="https://www.linkedin.com/in/navnoorbawa/" target="_blank" style="color: white; text-decoration: none; font-size: 24px; font-weight: 500;">Navnoor Bawa</a>
+        </div>
+    """, unsafe_allow_html=True)
     
     try:
         # Setup sidebar and get parameters
@@ -1663,6 +1672,5 @@ def main():
         if st.checkbox("Show detailed error trace", key="show_error"):
             st.exception(e)
 
-# Run the application
 if __name__ == "__main__":
     main()
